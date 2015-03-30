63cb3617fe2004fcc971fd3bdbdeb5a2c5d91a38
Emmanuel Capelle <capelle.e@gmail.com>
2015-03-30 22:07:13 +0200

    Update TODO list

--- restserver/urls.py
+++ restserver/urls.py
@@ -5,7 +5,9 @@ from restserver import views
 #TODO: urls.py pour rest server (voir http://www.django-rest-framework.org/tutorial/quickstart/)
 
 urlpatterns = [
-    url(r'auth', views.RESTLogin.as_view()),
-    url(r'logout', views.RESTLogout.as_view()),
+    url(r'auth', views.RESTLogin.as_view(), name="REST-login"),
+    url(r'logout', views.RESTLogout.as_view(), name="REST-logout"),
+    
+    # Config
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
